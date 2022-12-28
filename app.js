a15cbc06410ace62a14897e8724fca338f41b6e9
Kunal Gaikar <86392134+kunalgaikar@users.noreply.github.com>
2022-12-28 11:25:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const form = document.querySelector('#searchForm');
+const res = document.querySelector('#tableResult');
+var upd ;
+form.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+    if(upd){
+        clearTimeout(upd);
+    }
+    const cType = form.elements.coinType.value;
+
+    fetchPrice(cType);
+
+});
+
+const fetchPrice= async(cType) =>{
+    const r = await axios.get(`https://api.cryptonator.com/api/ticker/${cType}`);
+    const price = r.data.ticker.price;
+    const volume = r.data.ticker.volume;
+    const change = r.data.ticker.change;
+    const base = r.data.ticker.base;
+    const target = r.data.ticker.target;
+    const time = r.data.timestamp;
+
+    res.innerHTML = `<tr style=""background-color:blue; color:white; font-weight:600">
+    <td>
+        Property
+    </td>
+    <td>value</td>
+</tr>
+<tr>
+    <td>
+        ${base}
+    </td>
+    <td>${price} ${target}</td>
+</tr>
+<tr>
+    <td>
+        Volume
+    </td>
+    <td>${volume}</td>
+</tr>
+<tr>
+    <td>
+        Change
+    </td>
+    <td>${change}</td>
+</tr>
+<tr>
+    <td>
+        Last Update
+    </td>
+    <td>${time}</td>
+</tr>`
+
+    upd = setTimeout(()=>fetchPrice(cType),10000);
+}
